20d58c63b45864ddb2ce9e8e4ce9fa5544fe1b0c
[Sarah Lang] <[lynnlang98@gmail.com]>
2022-11-14 21:09:27 -0500

    local storage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,6 @@
 var today = dayjs();
 $('#currentDay').text(today.format('dddd, MMM D, YYYY'));
 
-
 $('.time-block').each(function () {
   var blockHour = parseInt($(this).attr('id').split('-')[1]);
   if (blockHour < dayjs().hour()) {
@@ -22,18 +21,20 @@ saveBtn.on('click', function(event) {
   var input = $(event.target).siblings('textarea').val()
   var mainDiv = $(event.target).parent('div').attr('id')
   localStorage.setItem(mainDiv, input)
+  renderInput()
 })
 
 // makes the users saved input persist even when the page reloads
-var displayHr9 = $("#hour-9")
-var displayHr10 = $("#hour-10")
-var displayHr11 = $("#hour-11")
-var displayHr12 = $("#hour-12")
-var displayHr1 = $("#hour-13")
-var displayHr2 = $("#hour-14")
-var displayHr3 = $("#hour-15")
-var displayHr4 = $("#hour-16")
-var displayHr5 = $("#hour-17")
-// function renderInput() {
-//   var lastInput = $(this).parent().attr('id').split('hour-9')[0]);
-//   }
+function renderInput() {
+  var hr9 = $("#hour-9").siblings('textarea').text()
+  var hr10 = $("#hour-10").siblings('textarea').text()
+  var hr11 = $("#hour-11").siblings('textarea').text()
+  var hr12 = $("#hour-12").siblings('textarea').text()
+  var hr13 = $("#hour-13").siblings('textarea').text()
+  var hr14 = $("#hour-14").siblings('textarea').text()
+  var hr15 = $("#hour-15").siblings('textarea').text()
+  var hr16 = $("#hour-16").siblings('textarea').text()
+  var hr17 = $("#hour-17").siblings('textarea').text()
+  localStorage.getItem(hr9, hr10, hr11, hr12, hr13, hr14, hr15, hr16, hr17)
+  console.log(localStorage)
+}
