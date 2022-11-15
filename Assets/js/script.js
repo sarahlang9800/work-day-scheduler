c2261ae1ede7513c638a50a7d6b86d6adaf8e8b8
[Sarah Lang] <[lynnlang98@gmail.com]>
2022-11-14 21:49:23 -0500

    link JavaScript folder

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -26,15 +26,15 @@ saveBtn.on('click', function(event) {
 
 // makes the users saved input persist even when the page reloads
 function renderInput() {
-  var hr9 = $("#hour-9").siblings('textarea').text()
-  var hr10 = $("#hour-10").siblings('textarea').text()
-  var hr11 = $("#hour-11").siblings('textarea').text()
-  var hr12 = $("#hour-12").siblings('textarea').text()
-  var hr13 = $("#hour-13").siblings('textarea').text()
-  var hr14 = $("#hour-14").siblings('textarea').text()
-  var hr15 = $("#hour-15").siblings('textarea').text()
-  var hr16 = $("#hour-16").siblings('textarea').text()
-  var hr17 = $("#hour-17").siblings('textarea').text()
-  localStorage.getItem(hr9, hr10, hr11, hr12, hr13, hr14, hr15, hr16, hr17)
-  console.log(localStorage)
+  var hr9 = $("#hour-9 .description").val(localStorage.getItem('hour-9'))
+  var hr10 = $("#hour-10 .description").val(localStorage.getItem('hour-10'))
+  var hr11 = $("#hour-11 .description").val(localStorage.getItem('hour-11'))
+  var hr12 = $("#hour-12 .description").val(localStorage.getItem('hour-12'))
+  var hr13 = $("#hour-13 .description").val(localStorage.getItem('hour-13'))
+  var hr14 = $("#hour-14 .description").val(localStorage.getItem('hour-14'))
+  var hr15 = $("#hour-15 .description").val(localStorage.getItem('hour-15'))
+  var hr16 = $("#hour-16 .description").val(localStorage.getItem('hour-16'))
+  var hr17 = $("#hour-17 .description").val(localStorage.getItem('hour-17'))
+  window.localStorage.getItem(hr9, hr10, hr11, hr12, hr13, hr14, hr15, hr16, hr17)
 }
+renderInput()
